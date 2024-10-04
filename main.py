d20891caf83b8a9d18a317b35f0ed328df43cb58
viansebastian <viansbromokusumo@gmail.com>
2024-10-04 15:52:21 +0800

    modified

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import cv2
 # from backup.main_circles_processing import main_circles_pipeline
 from circles_usage_cy import main_circles_pipeline
 
-# answer1 = cv2.imread('inputs/circle_1/stu_good_light.jpg')
-# master_sheet = cv2.imread('inputs/circle_1/master_circle_best.jpg')
+answer1 = cv2.imread('inputs/circle_1/stu_good_light.jpg')
+master_sheet = cv2.imread('inputs/circle_1/master_circle_best.jpg')
 
-answer1 = cv2.imread('inputs/circle_2/student1_crop.jpg')
-master_sheet = cv2.imread('inputs/circle_2/master1_crop.jpg')
+# answer1 = cv2.imread('inputs/circle_2/student1_crop.jpg')
+# master_sheet = cv2.imread('inputs/circle_2/master1_crop.jpg')
 
 stu_final_score, stu_answer_key = main_circles_pipeline(master_sheet, answer1)
 
